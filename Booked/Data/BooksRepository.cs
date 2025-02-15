25d9ffa90b2f8d7a2b4f88ba36771b39064a74c0
BBualdo <mrseb10@gmail.com>
2025-02-15 17:55:44 +0100

    Declared Data Access Repository (not implemented Interface methods)

---
+++ Booked/Data/BooksRepository.cs
@@ -0,0 +1,43 @@
+﻿using Booked.Enums;
+using Booked.Model;
+
+namespace Booked.Data;
+
+public class BooksRepository : IBooksRepository
+{
+    // Initialize field for storing connection context
+    public BooksRepository()
+    {
+
+    }
+
+    public Task<IEnumerable<Book>> GetBooksAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book> GetBookAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddBookAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateBookAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteBookAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ToggleBookStatusAsync(int id, ReadingStatus newStatus)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Booked/Data/IBooksRepository.cs
@@ -0,0 +1,14 @@
+﻿using Booked.Enums;
+using Booked.Model;
+
+namespace Booked.Data;
+
+public interface IBooksRepository
+{
+    Task<IEnumerable<Book>> GetBooksAsync();
+    Task<Book> GetBookAsync(int id);
+    Task AddBookAsync(Book book);
+    Task UpdateBookAsync(Book book);
+    Task DeleteBookAsync(int id);
+    Task ToggleBookStatusAsync(int id, ReadingStatus newStatus);
+}
